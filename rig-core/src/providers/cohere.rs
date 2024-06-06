7585f0e7425755b82a37c79887553a03a58af7b7
Christophe <cvauclair@protonmail.com>
2024-06-06 17:15:43 -0400

    feat: Add model name enums for existing providers

--- rig-core/src/providers/cohere.rs
+++ rig-core/src/providers/cohere.rs
@@ -119,6 +119,21 @@ enum ApiResponse<T> {
 // ================================================================
 // Cohere Embedding API
 // ================================================================
+/// `embed-english-v3.0` embedding model
+pub const EMBED_ENGLISH_V3: &str = "embed-english-v3.0";
+/// `embed-english-light-v3.0` embedding model
+pub const EMBED_ENGLISH_LIGHT_V3: &str = "embed-english-light-v3.0";
+/// `embed-multilingual-v3.0` embedding model
+pub const EMBED_MULTILINGUAL_V3: &str = "embed-multilingual-v3.0";
+/// `embed-multilingual-light-v3.0` embedding model
+pub const EMBED_MULTILINGUAL_LIGHT_V3: &str = "embed-multilingual-light-v3.0";
+/// `embed-english-v2.0` embedding model
+pub const EMBED_ENGLISH_V2: &str = "embed-english-v2.0";
+/// `embed-english-light-v2.0` embedding model
+pub const EMBED_ENGLISH_LIGHT_V2: &str = "embed-english-light-v2.0";
+/// `embed-multilingual-v2.0` embedding model
+pub const EMBED_MULTILINGUAL_V2: &str = "embed-multilingual-v2.0";
+
 #[derive(Deserialize)]
 pub struct EmbeddingResponse {
     #[serde(default)]
@@ -226,6 +241,19 @@ impl EmbeddingModel {
 // ================================================================
 // Cohere Completion API
 // ================================================================
+/// `command-r-plus` completion model
+pub const COMMAND_R_PLUS: &str = "comman-r-plus";
+/// `command-r` completion model
+pub const COMMAND_R: &str = "command-r";
+/// `command` completion model
+pub const COMMAND: &str = "command";
+/// `command-nightly` completion model
+pub const COMMAND_NIGHTLY: &str = "command-nightly";
+/// `command-light` completion model
+pub const COMMAND_LIGHT: &str = "command-light";
+/// `command-light-nightly` completion model
+pub const COMMAND_LIGHT_NIGHTLY: &str = "command-light-nightly";
+
 #[derive(Debug, Deserialize)]
 pub struct CompletionResponse {
     pub text: String,
--- rig-core/src/providers/openai.rs
+++ rig-core/src/providers/openai.rs
@@ -116,6 +116,13 @@ enum ApiResponse<T> {
 // ================================================================
 // OpenAI Embedding API
 // ================================================================
+/// `text-embedding-3-large` embedding model
+pub const TEXT_EMBEDDING_3_LARGE: &str = "text-embedding-3-large";
+/// `text-embedding-3-small` embedding model
+pub const TEXT_EMBEDDING_3_SMALL: &str = "text-embedding-3-small";
+/// `text-embedding-ada-002` embedding model
+pub const TEXT_EMBEDDING_ADA_002: &str = "text-embedding-ada-002";
+
 #[derive(Debug, Deserialize)]
 pub struct EmbeddingResponse {
     pub object: String,
@@ -212,6 +219,41 @@ impl EmbeddingModel {
 // ================================================================
 // OpenAI Completion API
 // ================================================================
+/// `gpt-4o` completion model
+pub const GPT_4O: &str = "gpt-4o";
+/// `gpt-4o-2024-05-13` completion model
+pub const GPT_4O_2024_05_13: &str = "gpt-4o-2024-05-13";
+/// `gpt-4-turbo` completion model
+pub const GPT_4_TURBO: &str = "gpt-4-turbo";
+/// `gpt-4-turbo-2024-04-09` completion model
+pub const GPT_4_TURBO_2024_04_09: &str = "gpt-4-turbo-2024-04-09";
+/// `gpt-4-turbo-preview` completion model
+pub const GPT_4_TURBO_PREVIEW: &str = "gpt-4-turbo-preview";
+/// `gpt-4-0125-preview` completion model
+pub const GPT_4_0125_PREVIEW: &str = "gpt-4-0125-preview";
+/// `gpt-4-1106-preview` completion model
+pub const GPT_4_1106_PREVIEW: &str = "gpt-4-1106-preview";
+/// `gpt-4-vision-preview` completion model
+pub const GPT_4_VISION_PREVIEW: &str = "gpt-4-vision-preview";
+/// `gpt-4-1106-vision-preview` completion model
+pub const GPT_4_1106_VISION_PREVIEW: &str = "gpt-4-1106-vision-preview";
+/// `gpt-4` completion model
+pub const GPT_4: &str = "gpt-4";
+/// `gpt-4-0613` completion model
+pub const GPT_4_0613: &str = "gpt-4-0613";
+/// `gpt-4-32k` completion model
+pub const GPT_4_32K: &str = "gpt-4-32k";
+/// `gpt-4-32k-0613` completion model
+pub const GPT_4_32K_0613: &str = "gpt-4-32k-0613";
+/// `gpt-3.5-turbo` completion model
+pub const GPT_35_TURBO: &str = "gpt-3.5-turbo";
+/// `gpt-3.5-turbo-0125` completion model
+pub const GPT_35_TURBO_0125: &str = "gpt-3.5-turbo-0125";
+/// `gpt-3.5-turbo-1106` completion model
+pub const GPT_35_TURBO_1106: &str = "gpt-3.5-turbo-1106";
+/// `gpt-3.5-turbo-instruct` completion model
+pub const GPT_35_TURBO_INSTRUCT: &str = "gpt-3.5-turbo-instruct";
+
 #[derive(Debug, Deserialize)]
 pub struct CompletionResponse {
     pub id: String,
