bb712e3fdabfffbf4a4e6c35fb2eec951ff5c83f
Garance <garance.mary@gmail.com>
2024-10-09 15:49:00 -0400

    docs: update emebddings builder docstrings

--- rig-core/src/embeddings.rs
+++ rig-core/src/embeddings.rs
@@ -8,31 +8,66 @@
 //! struct, which allows users to build collections of document embeddings using different embedding
 //! models and document sources.
 //!
-//! The module also defines the [Embedding] struct, which represents a single document embedding,
-//! and the [DocumentEmbeddings] struct, which represents a document along with its associated
-//! embeddings. These structs are used to store and manipulate collections of document embeddings.
+//! The module also defines the [Embedding] struct, which represents a single document embedding.
+//!
+//! The module also defines the [Embeddable] trait, which represents types that can be embedded.
+//! Only types that implement the Embeddable trait can be used with the EmbeddingsBuilder.
 //!
 //! Finally, the module defines the [EmbeddingError] enum, which represents various errors that
 //! can occur during embedding generation or processing.
 //!
 //! # Example
 //! ```rust
-//! use rig::providers::openai::{Client, self};
-//! use rig::embeddings::{EmbeddingModel, EmbeddingsBuilder};
+//! use std::env;
 //!
-//! // Initialize the OpenAI client
-//! let openai = Client::new("your-openai-api-key");
+//! use rig::{
+//!    embeddings::EmbeddingsBuilder,
+//!    providers::openai::{Client, TEXT_EMBEDDING_ADA_002},
+//! };
+//! use rig_derive::Embed;
 //!
-//! // Create an instance of the `text-embedding-ada-002` model
-//! let embedding_model = openai.embedding_model(openai::TEXT_EMBEDDING_ADA_002);
+//! #[derive(Embed)]
+//! struct FakeDefinition {
+//!    id: String,
+//!    word: String,
+//!    #[embed]
+//!    definitions: Vec<String>,
+//! }
+//! // Create OpenAI client
+//! let openai_api_key = env::var("OPENAI_API_KEY").expect("OPENAI_API_KEY not set");
+//! let openai_client = Client::new(&openai_api_key);
 //!
-//! // Create an embeddings builder and add documents
-//! let embeddings = EmbeddingsBuilder::new(embedding_model)
-//!     .simple_document("doc1", "This is the first document.")                                                                                                         
-//!     .simple_document("doc2", "This is the second document.")
-//!     .build()
-//!     .await
-//!     .expect("Failed to build embeddings.");
+//! let model = openai_client.embedding_model(TEXT_EMBEDDING_ADA_002);
+//!
+//! let embeddings = EmbeddingsBuilder::new(model.clone())
+//!    .documents(vec![
+//!        FakeDefinition {
+//!            id: "doc0".to_string(),
+//!            word: "flurbo".to_string(),
+//!            definitions: vec![
+//!                "A green alien that lives on cold planets.".to_string(),
+//!                "A fictional digital currency that originated in the animated series Rick and Morty.".to_string()
+//!            ]
+//!        },
+//!        FakeDefinition {
+//!            id: "doc1".to_string(),
+//!            word: "glarb-glarb".to_string(),
+//!            definitions: vec![
+//!                "An ancient tool used by the ancestors of the inhabitants of planet Jiro to farm the land.".to_string(),
+//!                "A fictional creature found in the distant, swampy marshlands of the planet Glibbo in the Andromeda galaxy.".to_string()
+//!            ]
+//!        },
+//!        FakeDefinition {
+//!            id: "doc2".to_string(),
+//!            word: "linglingdong".to_string(),
+//!            definitions: vec![
+//!                "A term used by inhabitants of the sombrero galaxy to describe humans.".to_string(),
+//!                "A rare, mystical instrument crafted by the ancient monks of the Nebulon Mountain Ranges on the planet Quarm.".to_string()
+//!            ]
+//!        },
+//!    ])
+//!    .build()
+//!    .await?;
 //!                                 
 //! // Use the generated embeddings
 //! // ...
@@ -102,7 +137,7 @@ pub trait EmbeddingModel: Clone + Sync + Send {
 /// Struct that holds a single document and its embedding.
 #[derive(Clone, Default, Deserialize, Serialize, Debug)]
 pub struct Embedding {
-    /// The document that was embedded
+    /// The document that was embedded. Used for debugging.
     pub document: String,
     /// The embedding vector
     pub vec: Vec<f64>,
@@ -159,6 +194,7 @@ impl<M: EmbeddingModel, D: Embeddable<Kind = K>, K: EmbeddingKind> EmbeddingsBui
         }
     }
 
+    /// Add a document that implements `Embeddable` to the builder.
     pub fn document(mut self, document: D) -> Self {
         let embed_targets = document.embeddable();
 
@@ -166,6 +202,7 @@ impl<M: EmbeddingModel, D: Embeddable<Kind = K>, K: EmbeddingKind> EmbeddingsBui
         self
     }
 
+    /// Add many documents that implement `Embeddable` to the builder.
     pub fn documents(mut self, documents: Vec<D>) -> EmbeddingsBuilder<M, D, D::Kind> {
         documents.into_iter().for_each(|doc| {
             let embed_targets = doc.embeddable();
@@ -180,7 +217,11 @@ impl<M: EmbeddingModel, D: Embeddable<Kind = K>, K: EmbeddingKind> EmbeddingsBui
 impl<M: EmbeddingModel, D: Embeddable + Send + Sync + Clone>
     EmbeddingsBuilder<M, D, ManyEmbedding>
 {
+    /// Generate embeddings for all documents in the builder.
+    /// The method only applies when documents in the builder each contain multiple embedding targets.
+    /// Returns a vector of tuples, where the first element is the document and the second element is the vector of embeddings.
     pub async fn build(&self) -> Result<Vec<(D, Vec<Embedding>)>, EmbeddingError> {
+        // Use this for reference later to merge a document back with its embeddings.
         let documents_map = self
             .documents
             .clone()
@@ -189,22 +230,19 @@ impl<M: EmbeddingModel, D: Embeddable + Send + Sync + Clone>
             .map(|(id, (document, _))| (id, document))
             .collect::<HashMap<_, _>>();
 
-        let embeddings = stream::iter(self.documents.clone().into_iter().enumerate())
-            // Flatten the documents
+        let embeddings = stream::iter(self.documents.iter().enumerate())
+            // Merge the embedding targets of each document into a single list of embedding targets.
             .flat_map(|(i, (_, embed_targets))| {
-                stream::iter(
-                    embed_targets
-                        .into_iter()
-                        .map(move |target| (i, target.clone())),
-                )
+                stream::iter(embed_targets.iter().map(move |target| (i, target.clone())))
             })
-            // Chunk them into N (the emebdding API limit per request)
+            // Chunk them into N (the emebdding API limit per request).
             .chunks(M::MAX_DOCUMENTS)
-            // Generate the embeddings
+            // Generate the embeddings for a chunk at a time.
             .map(|docs| async {
-                let (documents, embed_targets): (Vec<_>, Vec<_>) = docs.into_iter().unzip();
+                let (document_indices, embed_targets): (Vec<_>, Vec<_>) = docs.into_iter().unzip();
+                
                 Ok::<_, EmbeddingError>(
-                    documents
+                    document_indices
                         .into_iter()
                         .zip(self.model.embed_documents(embed_targets).await?.into_iter())
                         .collect::<Vec<_>>(),
@@ -213,8 +251,6 @@ impl<M: EmbeddingModel, D: Embeddable + Send + Sync + Clone>
             .boxed()
             // Parallelize the embeddings generation over 10 concurrent requests
             .buffer_unordered(max(1, 1024 / M::MAX_DOCUMENTS))
-            // .try_collect::<Vec<_>>()
-            // .await;
             .try_fold(
                 HashMap::new(),
                 |mut acc: HashMap<_, Vec<_>>, embeddings| async move {
@@ -242,6 +278,9 @@ impl<M: EmbeddingModel, D: Embeddable + Send + Sync + Clone>
 impl<M: EmbeddingModel, D: Embeddable + Send + Sync + Clone>
     EmbeddingsBuilder<M, D, SingleEmbedding>
 {
+    /// Generate embeddings for all documents in the builder.
+    /// The method only applies when documents in the builder each contain a single embedding target.
+    /// Returns a vector of tuples, where the first element is the document and the second element is the embedding.
     pub async fn build(&self) -> Result<Vec<(D, Embedding)>, EmbeddingError> {
         let embeddings = stream::iter(
             self.documents
@@ -274,6 +313,9 @@ impl<M: EmbeddingModel, D: Embeddable + Send + Sync + Clone>
     }
 }
 
+//////////////////////////////////////////////////////
+/// Implementations of Embeddable for common types ///
+//////////////////////////////////////////////////////
 impl Embeddable for String {
     type Kind = SingleEmbedding;
 
@@ -282,7 +324,71 @@ impl Embeddable for String {
     }
 }
 
+impl Embeddable for i8 {
+    type Kind = SingleEmbedding;
+
+    fn embeddable(&self) -> Vec<String> {
+        vec![self.to_string()]
+    }
+}
+
+impl Embeddable for i16 {
+    type Kind = SingleEmbedding;
+
+    fn embeddable(&self) -> Vec<String> {
+        vec![self.to_string()]
+    }
+}
+
 impl Embeddable for i32 {
+    type Kind = SingleEmbedding;
+
+    fn embeddable(&self) -> Vec<String> {
+        vec![self.to_string()]
+    }
+}
+
+impl Embeddable for i64 {
+    type Kind = SingleEmbedding;
+
+    fn embeddable(&self) -> Vec<String> {
+        vec![self.to_string()]
+    }
+}
+
+impl Embeddable for i128 {
+    type Kind = SingleEmbedding;
+
+    fn embeddable(&self) -> Vec<String> {
+        vec![self.to_string()]
+    }
+}
+
+impl Embeddable for f32 {
+    type Kind = SingleEmbedding;
+
+    fn embeddable(&self) -> Vec<String> {
+        vec![self.to_string()]
+    }
+}
+
+impl Embeddable for f64 {
+    type Kind = SingleEmbedding;
+
+    fn embeddable(&self) -> Vec<String> {
+        vec![self.to_string()]
+    }
+}
+
+impl Embeddable for bool {
+    type Kind = SingleEmbedding;
+
+    fn embeddable(&self) -> Vec<String> {
+        vec![self.to_string()]
+    }
+}
+
+impl Embeddable for char {
     type Kind = SingleEmbedding;
 
     fn embeddable(&self) -> Vec<String> {
