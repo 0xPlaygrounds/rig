3e1ff232af223198e5cd63e61a91c42f8dc0e28a
Garance <garance.mary@gmail.com>
2024-11-07 16:59:46 -0500

    docs: fix hyperlink

--- rig-core/rig-core-derive/src/lib.rs
+++ rig-core/rig-core-derive/src/lib.rs
@@ -9,8 +9,8 @@ mod embed;
 pub(crate) const EMBED: &str = "embed";
 
 /// References:
-/// https://doc.rust-lang.org/book/ch19-06-macros.html#how-to-write-a-custom-derive-macro
-/// https://doc.rust-lang.org/reference/procedural-macros.html
+/// <https://doc.rust-lang.org/book/ch19-06-macros.html#how-to-write-a-custom-derive-macro>
+/// <https://doc.rust-lang.org/reference/procedural-macros.html>
 #[proc_macro_derive(Embed, attributes(embed))]
 pub fn derive_embedding_trait(item: TokenStream) -> TokenStream {
     let mut input = parse_macro_input!(item as DeriveInput);
