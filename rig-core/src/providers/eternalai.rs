6923064e6939507a94637c023c5c2a57e68cf857
tedison <timothyedison@hotmail.com>
2025-01-17 22:06:25 +0100

    fix after rebase

--- rig-core/src/providers/eternalai.rs
+++ rig-core/src/providers/eternalai.rs
@@ -369,6 +369,7 @@ impl TryFrom<CompletionResponse> for completion::CompletionResponse<CompletionRe
                 Ok(completion::CompletionResponse {
                     choice: completion::ModelChoice::ToolCall(
                         call.function.name.clone(),
+                        call.id.clone(),
                         serde_json::from_str(&call.function.arguments)?,
                     ),
                     raw_response: value,
