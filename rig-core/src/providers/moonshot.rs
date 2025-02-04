52eec3a614bdb7d1331ec1a8261ecfa44c275136
threewebcode <phpjavaphp@gmail.com>
2025-02-04 22:15:33 +0800

    chore: add config attribute macro

--- rig-core/src/providers/moonshot.rs
+++ rig-core/src/providers/moonshot.rs
@@ -207,6 +207,7 @@ impl CompletionModel {
 impl completion::CompletionModel for CompletionModel {
     type Response = CompletionResponse;
 
+    #[cfg_attr(feature = "worker", worker::send)]
     async fn completion(
         &self,
         mut completion_request: CompletionRequest,
