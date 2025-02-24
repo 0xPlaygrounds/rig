0ff042cd431e90fc5f7b3926e31b22ced5d9f670
Lin <35210535+451846939@users.noreply.github.com>
2025-02-25 07:32:42 +0800

    feat: add ollama client (#285)
    * add ollama client
    
    * update ollama client completion params
    
    * fix rag and chat
    
    * cargo fmt
    
    * cargo fmt
    
    * add test & fix tools call
    
    * fmt code
    
    * fix cargo check
    
    * remove Infallible From
    
    * use ollama client
    
    * add ollama examples
    
    * add ollama examples
    
    * cargo fmt image_ollama
    
    * style(ollama): typos / formatting
    
    * rename image
    
    ---------
    
    Co-authored-by: 0xMochan <mochanbot@gmail.com>

--- rig-core/examples/agent_with_ollama.rs
+++ rig-core/examples/agent_with_ollama.rs
@@ -3,13 +3,12 @@ use rig::{completion::Prompt, providers};
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
-    // Create an OpenAI client with a custom base url, a local ollama endpoint
-    // The API Key is unnecessary for most local endpoints
-    let client = providers::openai::Client::from_url("ollama", "http://localhost:11434/v1");
+    // Create ollama client
+    let client = providers::ollama::Client::new();
 
     // Create agent with a single context prompt
     let comedian_agent = client
-        .agent("llama3.2:latest")
+        .agent("qwen2.5:14b")
         .preamble("You are a comedian here to entertain the user using humour and jokes.")
         .build();
 
