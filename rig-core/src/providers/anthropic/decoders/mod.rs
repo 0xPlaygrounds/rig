e830f270a9ae2512190cfc24d7d56d549d900256
Piotr Ostrowski <piotr.jp.ostrowski@gmail.com>
2025-03-04 00:17:01 +0100

    rust port the anthropic sdk implementation

---
+++ rig-core/src/providers/anthropic/decoders/mod.rs
@@ -0,0 +1,9 @@
+/*
+ * The code from this module is a Rust port of the
+ * https://github.com/anthropics/anthropic-sdk-typescript/tree/main decoders
+ *
+ * The original code is licensed under PBT license
+ * https://github.com/anthropics/anthropic-sdk-typescript/blob/main/LICENSE
+ */
+pub mod jsonl;
+pub mod line;
