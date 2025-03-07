398961c7fa9da378c59d51a75595275f0bcc178b
piotrostr <piotr.ostrowski@aol.com>
2025-03-07 01:25:42 +0100

    Update mod.rs

--- rig-core/src/providers/anthropic/decoders/mod.rs
+++ rig-core/src/providers/anthropic/decoders/mod.rs
@@ -2,7 +2,7 @@
  * The code from this module is a Rust port of the
  * https://github.com/anthropics/anthropic-sdk-typescript/tree/main decoders
  *
- * The original code is licensed under PBT license
+ * The original code is licensed under MIT license
  * https://github.com/anthropics/anthropic-sdk-typescript/blob/main/LICENSE
  */
 pub mod jsonl;
