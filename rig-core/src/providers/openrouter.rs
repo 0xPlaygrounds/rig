cc38cdeea5c75df7115e4d79078322788255d9ca
Joshua Mo <joshua.mo.876@gmail.com>
2025-03-10 15:52:36 +0000

    refactor: add some model consts

--- rig-core/src/providers/openrouter.rs
+++ rig-core/src/providers/openrouter.rs
@@ -144,32 +144,16 @@ impl std::fmt::Display for Usage {
 }
 
 // ================================================================
-// openrouter Completion API
+// OpenRouter Completion API
 // ================================================================
-/// Meta Llama 3.1b Instruct model with 8B parameters.
-pub const LLAMA_3_1_8B: &str = "meta-llama/Meta-Llama-3.1-8B-Instruct";
-/// Meta Llama 3.3b Instruct model with 70B parameters.
-pub const LLAMA_3_3_70B: &str = "meta-llama/Llama-3.3-70B-Instruct";
-/// Meta Llama 3.1b Instruct model with 70B parameters.
-pub const LLAMA_3_1_70B: &str = "meta-llama/Meta-Llama-3.1-70B-Instruct";
-/// Meta Llama 3 Instruct model with 70B parameters.
-pub const LLAMA_3_70B: &str = "meta-llama/Meta-Llama-3-70B-Instruct";
-/// Hermes 3 Instruct model with 70B parameters.
-pub const HERMES_3_70B: &str = "NousResearch/Hermes-3-Llama-3.1-70b";
-/// Deepseek v2.5 model.
-pub const DEEPSEEK_2_5: &str = "deepseek-ai/DeepSeek-V2.5";
-/// Qwen 2.5 model with 72B parameters.
-pub const QWEN_2_5_72B: &str = "Qwen/Qwen2.5-72B-Instruct";
-/// Meta Llama 3.2b Instruct model with 3B parameters.
-pub const LLAMA_3_2_3B: &str = "meta-llama/Llama-3.2-3B-Instruct";
-/// Qwen 2.5 Coder Instruct model with 32B parameters.
-pub const QWEN_2_5_CODER_32B: &str = "Qwen/Qwen2.5-Coder-32B-Instruct";
-/// Preview (latest) version of Qwen model with 32B parameters.
-pub const QWEN_QWQ_PREVIEW_32B: &str = "Qwen/QwQ-32B-Preview";
-/// Deepseek R1 Zero model.
-pub const DEEPSEEK_R1_ZERO: &str = "deepseek-ai/DeepSeek-R1-Zero";
-/// Deepseek R1 model.
-pub const DEEPSEEK_R1: &str = "deepseek-ai/DeepSeek-R1";
+/// The `qwen/qwq-32b` model. Find more models at <https://openrouter.ai/models>.
+const QWEN_QWQ_32B: &str = "qwen/qwq-32b";
+/// The `anthropic/claude-3.7-sonnet` model. Find more models at <https://openrouter.ai/models>.
+const CLAUDE_3_7_SONNET: &str = "anthropic/claude-3.7-sonnet";
+/// The `perplexity/sonar-pro` model. Find more models at <https://openrouter.ai/models>.
+const PERPLEXITY_SONAR_PRO: &str = "perplexity/sonar-pro";
+/// The `google/gemini-2.0-flash-001` model. Find more models at <https://openrouter.ai/models>.
+const GEMINI_FLASH_2_0: &str = "google/gemini-2.0-flash-001";
 
 /// A openrouter completion object.
 ///
