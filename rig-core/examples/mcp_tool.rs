29d3d7a82502c79b370609085474905e1707c8ef
stevo <spolin44@gmail.com>
2025-03-13 13:20:08 -0400

    updated deps and example

--- rig-core/examples/mcp_tool.rs
+++ rig-core/examples/mcp_tool.rs
@@ -1,75 +1,25 @@
-use serde_json::json;
-
+use anyhow::Result;
+use mcp_core::{
+    client::ClientBuilder,
+    server::Server,
+    tool_text_content,
+    transport::{ClientSseTransportBuilder, ServerSseTransport},
+    types::{ClientCapabilities, Implementation, ServerCapabilities, ToolResponseContent},
+};
+use mcp_core_macros::tool;
 use rig::{
     completion::Prompt,
     providers::{self},
 };
+use serde_json::json;
 
-use mcp_core::{
-    client::ClientBuilder,
-    server::Server,
-    tool_error_response, tool_text_response,
-    tools::ToolHandlerFn,
-    transport::{ClientSseTransportBuilder, ServerSseTransport},
-    types::{
-        CallToolRequest, CallToolResponse, ClientCapabilities, Implementation, ServerCapabilities,
-        Tool, ToolResponseContent,
-    },
-};
-
-pub struct AddTool;
-
-impl AddTool {
-    pub fn tool() -> Tool {
-        Tool {
-            name: "Add".to_string(),
-            description: Some("Adds two numbers together.".to_string()),
-            input_schema: serde_json::json!({
-                    "type": "object",
-                    "properties": {
-                        "a": {
-                            "type": "number",
-                            "description": "The first number to add"
-                        },
-                        "b": {
-                            "type": "number",
-                            "description": "The second number to add"
-                        }
-                    },
-                    "required": [
-                        "a",
-                        "b"
-                    ]
-            }),
-        }
-    }
-
-    pub async fn call() -> ToolHandlerFn {
-        move |req: CallToolRequest| {
-            Box::pin(async move {
-                let args = req.arguments.unwrap_or_default();
-
-                let a = match args["a"].as_f64() {
-                    Some(val) => val,
-                    None => {
-                        return tool_error_response!(anyhow::anyhow!(
-                            "Missing or invalid 'a' parameter"
-                        ))
-                    }
-                };
-                let b = match args["b"].as_f64() {
-                    Some(val) => val,
-                    None => {
-                        return tool_error_response!(anyhow::anyhow!(
-                            "Missing or invalid 'b' parameter"
-                        ))
-                    }
-                };
-
-                tool_text_response!((a + b).to_string())
-            })
-        }
-    }
+#[tool(
+    name = "Add",
+    description = "Adds two numbers together.",
+    params(a = "The first number to add", b = "The second number to add")
+)]
+async fn add_tool(a: f64, b: f64) -> Result<ToolResponseContent> {
+    Ok(tool_text_content!((a + b).to_string()))
 }
 
 #[tokio::main]
@@ -84,17 +34,15 @@ async fn main() -> Result<(), anyhow::Error> {
             })),
             ..Default::default()
         })
-        .register_tool(AddTool::tool(), AddTool::call().await)
+        .register_tool(AddTool::tool(), AddTool::call())
         .build();
     let mcp_server_transport =
         ServerSseTransport::new("127.0.0.1".to_string(), 3000, mcp_server_protocol);
-
-    // Start the MCP server in the background
     tokio::spawn(async move { Server::start(mcp_server_transport).await });
 
     // Create the MCP client
     let mcp_client = ClientBuilder::new(
-        ClientSseTransportBuilder::new("http://localhost:3000".to_string()).build(),
+        ClientSseTransportBuilder::new("http://127.0.0.1:3000/sse".to_string()).build(),
     )
     .build();
     // Start the MCP client
