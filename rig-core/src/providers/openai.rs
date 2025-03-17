d498c601657afd704c0d2c10b959e217845751e7
Joshua Mo <joshua.mo.876@gmail.com>
2025-03-17 11:10:58 +0000

    fix: amend deserialization test

--- rig-core/src/providers/openai.rs
+++ rig-core/src/providers/openai.rs
@@ -1164,7 +1164,7 @@ mod tests {
                     "text": "What's in this image?"
                 },
                 {
-                    "type": "image",
+                    "type": "image_url",
                     "image_url": {
                         "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg"
                     }
