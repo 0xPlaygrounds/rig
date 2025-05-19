6a81bc8c53bb345d3396e2d4d5b5deccfbe0490b
adrianncovaci <cpaadrian2@gmail.com>
2025-05-19 15:34:21 +0400

    fix: Fix `dims` value for gemini's `EMBEDDING_004` (#452) (#453)

--- rig-core/src/providers/gemini/embedding.rs
+++ rig-core/src/providers/gemini/embedding.rs
@@ -35,8 +35,7 @@ impl embeddings::EmbeddingModel for EmbeddingModel {
 
     fn ndims(&self) -> usize {
         match self.model.as_str() {
-            EMBEDDING_001 => 768,
-            EMBEDDING_004 => 1024,
+            EMBEDDING_001 | EMBEDDING_004 => 768,
             _ => 0, // Default to 0 for unknown models
         }
     }
