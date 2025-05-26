96eba39237d408b54b632ac2dfa5b731f042c50d
yavens <179155341+yavens@users.github.noreply.github.com>
2025-05-26 11:42:56 -0400

    fix: update mistral

--- rig-core/src/providers/mistral/client.rs
+++ rig-core/src/providers/mistral/client.rs
@@ -1,16 +1,15 @@
-use schemars::JsonSchema;
-use serde::{Deserialize, Serialize};
-
-use crate::{agent::AgentBuilder, extractor::ExtractorBuilder};
+use serde::Deserialize;
 
 use super::{
     embedding::{EmbeddingModel, MISTRAL_EMBED},
     CompletionModel,
 };
+use crate::client::{CompletionClient, EmbeddingsClient, ProviderClient};
+use crate::impl_conversion_traits;
 
 const MISTRAL_API_BASE_URL: &str = "https://api.mistral.ai";
 
-#[derive(Clone)]
+#[derive(Debug, Clone)]
 pub struct Client {
     base_url: String,
     http_client: reqwest::Client,
@@ -19,13 +18,6 @@ pub struct Client {
 impl Client {
     pub fn new(api_key: &str) -> Self {
         Self::from_url(api_key, MISTRAL_API_BASE_URL)
-    }
-
-    /// Create a new Mistral client from the `MISTRAL_API_KEY` environment variable.
-    /// Panics if the environment variable is not set.
-    pub fn from_env() -> Self {
-        let api_key = std::env::var("MISTRAL_API_KEY").expect("MISTRAL_API_KEY not set");
-        Self::new(&api_key)
     }
 
     pub fn from_url(api_key: &str, base_url: &str) -> Self {
@@ -47,30 +39,26 @@ impl Client {
         }
     }
 
-    /// Create an embedding model with the given name.
-    /// Note: default embedding dimension of 0 will be used if model is not known.
-    ///
-    /// # Example
-    /// ```
-    /// use rig::providers::mistral::{Client, self};
-    ///
-    /// // Initialize mistral client
-    /// let mistral = Client::new("your-mistral-api-key");
-    ///
-    /// let embedding_model = mistral.embedding_model(mistral::MISTRAL_EMBED);
-    /// ```
-    pub fn embedding_model(&self, model: &str) -> EmbeddingModel {
-        let ndims = match model {
-            MISTRAL_EMBED => 1024,
-            _ => 0,
-        };
-        EmbeddingModel::new(self.clone(), model, ndims)
-    }
-
     pub(crate) fn post(&self, path: &str) -> reqwest::RequestBuilder {
         let url = format!("{}/{}", self.base_url, path).replace("//", "/");
         self.http_client.post(url)
     }
+}
+
+impl ProviderClient for Client {
+    /// Create a new Mistral client from the `MISTRAL_API_KEY` environment variable.
+    /// Panics if the environment variable is not set.
+    fn from_env() -> Self
+    where
+        Self: Sized,
+    {
+        let api_key = std::env::var("MISTRAL_API_KEY").expect("MISTRAL_API_KEY not set");
+        Self::new(&api_key)
+    }
+}
+
+impl CompletionClient for Client {
+    type CompletionModel = CompletionModel;
 
     /// Create a completion model with the given name.
     ///
@@ -83,36 +71,40 @@ impl Client {
     ///
     /// let codestral = mistral.completion_model(mistral::CODESTRAL);
     /// ```
-    pub fn completion_model(&self, model: &str) -> CompletionModel {
+    fn completion_model(&self, model: &str) -> Self::CompletionModel {
         CompletionModel::new(self.clone(), model)
     }
+}
 
-    /// Create an agent builder with the given completion model.
+impl EmbeddingsClient for Client {
+    type EmbeddingModel = EmbeddingModel;
+
+    /// Create an embedding model with the given name.
+    /// Note: default embedding dimension of 0 will be used if model is not known.
     ///
     /// # Example
     /// ```
     /// use rig::providers::mistral::{Client, self};
     ///
-    /// // Initialize the Mistral client
+    /// // Initialize mistral client
     /// let mistral = Client::new("your-mistral-api-key");
     ///
-    /// let agent = mistral.agent(mistral::CODESTRAL)
-    ///    .preamble("You are comedian AI with a mission to make people laugh.")
-    ///    .temperature(0.0)
-    ///    .build();
+    /// let embedding_model = mistral.embedding_model(mistral::MISTRAL_EMBED);
     /// ```
-    pub fn agent(&self, model: &str) -> AgentBuilder<CompletionModel> {
-        AgentBuilder::new(self.completion_model(model))
+    fn embedding_model(&self, model: &str) -> EmbeddingModel {
+        let ndims = match model {
+            MISTRAL_EMBED => 1024,
+            _ => 0,
+        };
+        EmbeddingModel::new(self.clone(), model, ndims)
     }
 
-    /// Create an extractor builder with the given completion model.
-    pub fn extractor<T: JsonSchema + for<'a> Deserialize<'a> + Serialize + Send + Sync>(
-        &self,
-        model: &str,
-    ) -> ExtractorBuilder<T, CompletionModel> {
-        ExtractorBuilder::new(self.completion_model(model))
+    fn embedding_model_with_ndims(&self, model: &str, ndims: usize) -> Self::EmbeddingModel {
+        EmbeddingModel::new(self.clone(), model, ndims)
     }
 }
+
+impl_conversion_traits!(AsTranscription, AsAudioGeneration, AsImageGeneration for Client);
 
 #[derive(Clone, Debug, Deserialize)]
 pub struct Usage {
