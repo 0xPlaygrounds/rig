9c9d6091c2ee543cd00eca2cf181551420baf2a7
Joshua Mo <joshua.mo.876@gmail.com>
2025-06-22 18:11:33 +0100

    fix(rig-792): inconsistent implementations of with_custom_client

--- rig-core/src/providers/galadriel.rs
+++ rig-core/src/providers/galadriel.rs
@@ -72,6 +72,14 @@ impl Client {
         }
     }
 
+    /// Use your own `reqwest::Client`.
+    /// The default headers will be automatically attached upon trying to make a request.
+    pub fn with_custom_client(mut self, client: reqwest::Client) -> Self {
+        self.http_client = client;
+
+        self
+    }
+
     fn post(&self, path: &str) -> reqwest::RequestBuilder {
         let url = format!("{}/{}", self.base_url, path).replace("//", "/");
         let mut client = self.http_client.post(url).bearer_auth(&self.api_key);
