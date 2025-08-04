4fcbb3a3a31f2aa190230b853bd59e6ffee56104
Rajesh Jha <136908732+rajiknows@users.noreply.github.com>
2025-08-04 23:20:31 +0530

    feat: add `delete_tool` method to `Toolset` (#663)
    * feat: initialize foundry.rs
    
    * feat: foundry embeddings api
    
    * origin/main
    
    * feat: add delete method to toolset
    
    * added tests for tool deletion
    
    * clippy fix

--- rig-core/src/tool.rs
+++ rig-core/src/tool.rs
@@ -578,6 +578,10 @@ impl ToolSet {
             .insert(tool.name(), ToolType::Simple(Box::new(tool)));
     }
 
+    pub fn delete_tool(&mut self, tool_name: &str) {
+        let _ = self.tools.remove(tool_name);
+    }
+
     /// Merge another toolset into this one
     pub fn add_tools(&mut self, toolset: ToolSet) {
         self.tools.extend(toolset.tools);
@@ -681,5 +685,110 @@ impl ToolSetBuilder {
                 .map(|tool| (tool.name(), tool))
                 .collect(),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use serde_json::json;
+
+    use super::*;
+
+    #[test]
+    fn test_tool_deletion() {
+        let mut toolset = ToolSet::default();
+
+        #[derive(Deserialize)]
+        struct OperationArgs {
+            x: i32,
+            y: i32,
+        }
+
+        #[derive(Debug, thiserror::Error)]
+        #[error("Math error")]
+        struct MathError;
+
+        #[derive(Deserialize, Serialize)]
+        struct Adder;
+
+        impl Tool for Adder {
+            const NAME: &'static str = "add";
+            type Error = MathError;
+            type Args = OperationArgs;
+            type Output = i32;
+
+            async fn definition(&self, _prompt: String) -> ToolDefinition {
+                ToolDefinition {
+                    name: "add".to_string(),
+                    description: "Add x and y together".to_string(),
+                    parameters: json!({
+                        "type": "object",
+                        "properties": {
+                            "x": {
+                                "type": "number",
+                                "description": "The first number to add"
+                            },
+                            "y": {
+                                "type": "number",
+                                "description": "The second number to add"
+                            }
+                        },
+                        "required": ["x", "y"]
+                    }),
+                }
+            }
+
+            async fn call(&self, args: Self::Args) -> Result<Self::Output, Self::Error> {
+                let result = args.x + args.y;
+                Ok(result)
+            }
+        }
+
+        #[derive(Deserialize, Serialize)]
+        struct Subtract;
+
+        impl Tool for Subtract {
+            const NAME: &'static str = "subtract";
+            type Error = MathError;
+            type Args = OperationArgs;
+            type Output = i32;
+
+            async fn definition(&self, _prompt: String) -> ToolDefinition {
+                serde_json::from_value(json!({
+                    "name": "subtract",
+                    "description": "Subtract y from x (i.e.: x - y)",
+                    "parameters": {
+                        "type": "object",
+                        "properties": {
+                            "x": {
+                                "type": "number",
+                                "description": "The number to subtract from"
+                            },
+                            "y": {
+                                "type": "number",
+                                "description": "The number to subtract"
+                            }
+                        },
+                        "required": ["x", "y"]
+                    }
+                }))
+                .expect("Tool Definition")
+            }
+
+            async fn call(&self, args: Self::Args) -> Result<Self::Output, Self::Error> {
+                let result = args.x - args.y;
+                Ok(result)
+            }
+        }
+
+        toolset.add_tool(Adder);
+        toolset.add_tool(Subtract);
+
+        assert_eq!(toolset.tools.len(), 2);
+
+        toolset.delete_tool("add");
+
+        assert!(!toolset.contains("add"));
+        assert_eq!(toolset.tools.len(), 1);
     }
 }
