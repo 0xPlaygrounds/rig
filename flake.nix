4bafafe1616093f4d56b70547ff1e6a416cf7240
Fay Carsons <faycarsons23@gmail.com>
2025-09-17 16:17:42 -0400

    feat: Nix flake for development environment

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "Rig development environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            openssl
+            sqlite
+            postgresql
+            protobuf
+          ];
+
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+            cmake
+
+            cargo
+            rustc
+          ];
+
+          OPENSSL_DEV = pkgs.openssl.dev;
+          OPENSSL_LIB_DIR = "${pkgs.openssl.out}/lib";
+          OPENSSL_INCLUDE_DIR = "${pkgs.openssl.dev}/include";
+        };
+      }
+    );
+}
