50ec8fd1f1c70791a366fa6623165e6ee8a09917
Joshua Mo <102877324+joshua-mo-143@users.noreply.github.com>
2025-12-11 01:08:55 +0000

    feat(rig-1059): support `reqwest-middleware` client (#1152)

--- rig/rig-core/src/http_client/mod.rs
+++ rig/rig-core/src/http_client/mod.rs
@@ -270,3 +270,148 @@ impl HttpClientExt for reqwest::Client {
         }
     }
 }
+
+#[cfg(feature = "reqwest-middleware")]
+#[cfg_attr(docsrs, doc(cfg(feature = "reqwest-middleware")))]
+impl HttpClientExt for reqwest_middleware::ClientWithMiddleware {
+    fn send<T, U>(
+        &self,
+        req: Request<T>,
+    ) -> impl Future<Output = Result<Response<LazyBody<U>>>> + WasmCompatSend + 'static
+    where
+        T: Into<Bytes>,
+        U: From<Bytes> + WasmCompatSend,
+    {
+        let (parts, body) = req.into_parts();
+        let req = self
+            .request(parts.method, parts.uri.to_string())
+            .headers(parts.headers)
+            .body(body.into());
+
+        async move {
+            let response = req.send().await.map_err(instance_error)?;
+            if !response.status().is_success() {
+                return Err(Error::InvalidStatusCodeWithMessage(
+                    response.status(),
+                    response.text().await.unwrap(),
+                ));
+            }
+
+            let mut res = Response::builder().status(response.status());
+
+            if let Some(hs) = res.headers_mut() {
+                *hs = response.headers().clone();
+            }
+
+            let body: LazyBody<U> = Box::pin(async {
+                let bytes = response
+                    .bytes()
+                    .await
+                    .map_err(|e| Error::Instance(e.into()))?;
+
+                let body = U::from(bytes);
+                Ok(body)
+            });
+
+            res.body(body).map_err(Error::Protocol)
+        }
+    }
+
+    fn send_multipart<U>(
+        &self,
+        req: Request<MultipartForm>,
+    ) -> impl Future<Output = Result<Response<LazyBody<U>>>> + WasmCompatSend + 'static
+    where
+        U: From<Bytes>,
+        U: WasmCompatSend + 'static,
+    {
+        let (parts, body) = req.into_parts();
+        let body = reqwest::multipart::Form::from(body);
+
+        let req = self
+            .request(parts.method, parts.uri.to_string())
+            .headers(parts.headers)
+            .multipart(body);
+
+        async move {
+            let response = req.send().await.map_err(instance_error)?;
+            if !response.status().is_success() {
+                return Err(Error::InvalidStatusCodeWithMessage(
+                    response.status(),
+                    response.text().await.unwrap(),
+                ));
+            }
+
+            let mut res = Response::builder().status(response.status());
+
+            if let Some(hs) = res.headers_mut() {
+                *hs = response.headers().clone();
+            }
+
+            let body: LazyBody<U> = Box::pin(async {
+                let bytes = response
+                    .bytes()
+                    .await
+                    .map_err(|e| Error::Instance(e.into()))?;
+
+                let body = U::from(bytes);
+                Ok(body)
+            });
+
+            res.body(body).map_err(Error::Protocol)
+        }
+    }
+
+    fn send_streaming<T>(
+        &self,
+        req: Request<T>,
+    ) -> impl Future<Output = Result<StreamingResponse>> + WasmCompatSend
+    where
+        T: Into<Bytes>,
+    {
+        let (parts, body) = req.into_parts();
+
+        let req = self
+            .request(parts.method, parts.uri.to_string())
+            .headers(parts.headers)
+            .body(body.into())
+            .build()
+            .map_err(|x| Error::Instance(x.into()))
+            .unwrap();
+
+        let client = self.clone();
+
+        async move {
+            let response: reqwest::Response = client.execute(req).await.map_err(instance_error)?;
+            if !response.status().is_success() {
+                return Err(Error::InvalidStatusCodeWithMessage(
+                    response.status(),
+                    response.text().await.unwrap(),
+                ));
+            }
+
+            #[cfg(not(target_family = "wasm"))]
+            let mut res = Response::builder()
+                .status(response.status())
+                .version(response.version());
+
+            #[cfg(target_family = "wasm")]
+            let mut res = Response::builder().status(response.status());
+
+            if let Some(hs) = res.headers_mut() {
+                *hs = response.headers().clone();
+            }
+
+            use futures::StreamExt;
+
+            let mapped_stream: Pin<Box<dyn WasmCompatSendStream<InnerItem = Result<Bytes>>>> =
+                Box::pin(
+                    response
+                        .bytes_stream()
+                        .map(|chunk| chunk.map_err(|e| Error::Instance(Box::new(e)))),
+                );
+
+            res.body(mapped_stream).map_err(Error::Protocol)
+        }
+    }
+}
