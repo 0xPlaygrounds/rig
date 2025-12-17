a3050b745e284089eb2f99adfb16b74d4bad4514
Simon Janssen <janssen.simon@posteo.de>
2025-12-17 07:28:59 +0100

    cargo fmt

--- rig/rig-core/src/client/mod.rs
+++ rig/rig-core/src/client/mod.rs
@@ -521,10 +521,7 @@ where
 
     /// Set the HTTP headers used in this client
     pub fn http_headers(self, headers: HeaderMap) -> Self {
-        Self {
-            headers,
-            ..self
-        }
+        Self { headers, ..self }
     }
 
     pub(crate) fn headers_mut(&mut self) -> &mut HeaderMap {
